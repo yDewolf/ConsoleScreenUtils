02eab409026dbc62e6dd5b25398c9f1dc49b2cc4
yDewolf <andrersng@gmail.com>
2025-03-30 10:28:32 -0300

    Center a small 'cube' using 2 flex

--- src/java/main/com/github/ydewolf/consoleScreen/com/github/ydewolf/consoleScreen/Main.java
+++ src/java/main/com/github/ydewolf/consoleScreen/com/github/ydewolf/consoleScreen/Main.java
@@ -8,12 +8,14 @@ import enums.FlexDirection;
 
 public class Main {
     public static void main(String[] args) {
-        FlexWidget flex_widget = new FlexWidget(50, 15, FlexDirection.COLUMN);
+        FlexWidget flex_widget = new FlexWidget(5, 15, FlexDirection.COLUMN);
+        FlexWidget flex_widget2 = new FlexWidget(50, 15, FlexDirection.ROW);
         String[] mask = {"_", "o", "."};
         ConsoleScreen root = new ConsoleScreen(50, 15, mask);
-        root.addWidget(flex_widget);
+        flex_widget2.addWidget(flex_widget);
+        root.addWidget(flex_widget2);
 
-        Widget widget1 = new Widget(5, 5, true);
+        Widget widget1 = new Widget(5, 3, true);
         Widget widget2 = new Widget(5, 5, true);
         Widget widget3 = new Widget(5, 5, true);
         Widget widget4 = new Widget(5, 5, true);
@@ -23,6 +25,7 @@ public class Main {
         // flex_widget.addWidget(widget3);
         // flex_widget.addWidget(widget4);
         flex_widget.mapWidgets();
+        flex_widget2.mapWidgets();
         
         // int count = 0;
         // for (int y = 0; y < 10; y++) {
